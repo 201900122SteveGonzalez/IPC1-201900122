fa8f956c825abb3bb7ad23d3c155c5ce3f7305e2
steve estuvo aqui <hstevegonzacam@gmail.com>
2020-06-08 20:39:51 -0600

    ANEXO DE PUNTOS EXTRAS
    Anexo de puntos extras

---
+++ puntos extras/puntos_extra/src/puntos_extra/Puntos_extra.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puntos_extra;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Puntos_extra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        solicitar_seguro inicio = new solicitar_seguro();
+       inicio.setVisible(true);
+    }
+    
+}
