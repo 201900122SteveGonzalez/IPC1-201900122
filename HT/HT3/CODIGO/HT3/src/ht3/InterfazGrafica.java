007a3d4cd591868151dc251491e56342926d2c83
steve estuvo aqui <hstevegonzacam@gmail.com>
2020-06-25 02:02:37 -0600

    vetana hoja de trabajo 3

--- HT/HT3/CODIGO/HT3/src/ht3/InterfazGrafica.java
+++ HT/HT3/CODIGO/HT3/src/ht3/InterfazGrafica.java
@@ -1,31 +1,43 @@
 package ht3;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import javax.swing.*;
 /**
  *
  * @author steve gonzalez
  */
 public class InterfazGrafica  {
-JFrame Ventana_principal = new JFrame("Infinity");;
-JButton rojo= new JButton();
-JButton azul = new JButton();
-JLabel label1 = new JLabel("Rojo");
-JTextField text1 = new JTextField();
-JLabel label2 = new JLabel("Azul");
-JTextField text2 = new JTextField();
+FondoPanel fondo = new FondoPanel();
+JFrame Ventana_principal = new JFrame("Infinity");
+public static JButton rojo= new JButton();
+public static JButton azul = new JButton();
+JLabel label1 = new JLabel("ROJO");
+public static JTextField text1 = new JTextField();//rojo
+JLabel label2 = new JLabel("AZUL");
+public static JTextField text2 = new JTextField();//azul
+
     public InterfazGrafica() {
         Ventana_principal.setLayout(null);
         Ventana_principal.setBounds(340, 105, 570, 195);
         Ventana_principal.setVisible(true);   
         Ventana_principal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        Ventana_principal.setContentPane(fondo);
         rojo.setBounds(200,20,150,120);
-        rojo.setBackground(Color.red);
+        rojo.setBackground(Color.blue);
         Ventana_principal.setBackground(Color.YELLOW);
         azul.setBounds(25,20,150,120);
-        azul.setBackground(Color.blue);
-        label1.setBounds(385, 50, 80, 30);
+        azul.setBackground(Color.red);
+        label1.setBounds(385, 40, 90, 60);
+        Font f= new Font("ALGERIAN", Font.BOLD, 14); 
+        label1.setFont(f);
+        label1.setForeground(Color.WHITE);
+        label1.setVisible(true);
         text1.setBounds(430,50,90,30);
-        label2.setBounds(385,90, 80, 30);
+        label2.setBounds(385,80, 90, 60);
+        label2.setFont(f);
+        label2.setForeground(Color.WHITE);
         text2.setBounds(430,90,90,30);
         Ventana_principal.add(rojo);
         Ventana_principal.add(azul);
@@ -33,6 +45,19 @@ JTextField text2 = new JTextField();
         Ventana_principal.add(text1);
         Ventana_principal.add(label2);
         Ventana_principal.add(text2);
+        
     }
     
+    class FondoPanel extends JPanel {
+
+        private Image imagen;
+
+        @Override
+        public void paint(Graphics g) {
+            imagen = new ImageIcon(getClass().getResource("background.jpg")).getImage();
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+            setOpaque(false);
+            super.paint(g);
+        }
+    }
 }
