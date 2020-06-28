88ed745646dd6932c25874a4668b96874a509c5b
steve estuvo aqui <hstevegonzacam@gmail.com>
2020-06-27 22:56:53 -0600

    HT4
    hoja de trabajo 4

---
+++ HT/HT4/CODIGO/HT4/src/ht4/NodoCircular.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ht4;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class NodoCircular {
+    private int ID;
+    private String Valor;
+    private NodoCircular siguiente;
+
+    public NodoCircular(int ID, String Valor) {
+        this.ID = ID;
+        this.Valor = Valor;
+        this.siguiente = this;
+    }
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the Valor
+     */
+    public String getValor() {
+        return Valor;
+    }
+
+    /**
+     * @param Valor the Valor to set
+     */
+    public void setValor(String Valor) {
+        this.Valor = Valor;
+    }
+
+    /**
+     * @return the siguiente
+     */
+    public NodoCircular getSiguiente() {
+        return siguiente;
+    }
+
+    /**
+     * @param siguiente the siguiente to set
+     */
+    public void setSiguiente(NodoCircular siguiente) {
+        this.siguiente = siguiente;
+    }
+
+ 
+}
