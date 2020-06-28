b809f811a0711ce6d1790b901da06aeb70580f99
steve estuvo aqui <hstevegonzacam@gmail.com>
2020-06-27 23:47:36 -0600

    TAREA 4

---
+++ TAREAS/TAREA4/CODIGO/TAREA4/src/tarea4/TAREA4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class TAREA4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Graphviz interfaz = new Graphviz();
+    }
+    
+}
---
+++ TAREAS/TAREA4/CODIGO/TAREA4/src/tarea4/lista_circular.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4;
+
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class lista_circular {
+    private NodoCircular cabeza;
+
+    public lista_circular() {
+        this.cabeza=null;
+    }
+    
+    public void insertar (int ID, String Valor) {
+        //la cabeza se va moviendo de posicion
+        NodoCircular nuevo = new NodoCircular(ID, Valor);
+        if(this.getCabeza()!=null){
+            nuevo.setSiguiente(this.getCabeza().getSiguiente());
+            this.getCabeza().setSiguiente(nuevo);       
+        }
+        this.setCabeza(nuevo);      
+    }
+    
+    public void editar(int ID, String Valor){
+       NodoCircular actual =this.getCabeza();
+          while(actual.getSiguiente() != this.getCabeza()){     
+              if(actual.getID()==ID){
+                actual.setValor(Valor);
+            }
+          actual = actual.getSiguiente();
+        }
+        if(actual.getID()==ID){
+                actual.setValor(Valor);
+        }
+    }
+       
+    public void generar_listas(JTextArea text){  
+        text.setText("");
+         String encabezado = "digraph G { \n";
+         String cierre = "}";
+       int nodoActual = 1;
+                    String auxiliar="";
+          NodoCircular actual =this.getCabeza();
+          while(actual.getSiguiente() != this.getCabeza()){
+                      nodoActual++;
+              auxiliar = auxiliar+(("nodo" + (nodoActual-1) + " [label  = \"" + actual.getID() +","+actual.getValor() + "\"];\n"));
+              auxiliar = auxiliar+(("nodo" + (nodoActual-1)  + "->nodo" + nodoActual + "\n"));
+              actual = actual.getSiguiente();
+          }
+                      auxiliar = auxiliar+(("nodo" + (nodoActual) + " [label  = \"" + actual.getID() +","+actual.getValor() + "\"];\n"));
+                      auxiliar = auxiliar+(("nodo" + (nodoActual)  + "->nodo1"  + "\n")); 
+          String codigo = encabezado+auxiliar+cierre;
+        text.setText(codigo); 
+    }
+
+    /**
+     * @return the cabeza
+     */
+    public NodoCircular getCabeza() {
+        return cabeza;
+    }
+
+    /**
+     * @param cabeza the cabeza to set
+     */
+    public void setCabeza(NodoCircular cabeza) {
+        this.cabeza = cabeza;
+    }
+    
+    
+}
